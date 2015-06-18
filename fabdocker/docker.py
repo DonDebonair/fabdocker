9a432afeb1d5b659ba89bf8ed2d7a6857de4b9e6
Daan Debie <d.debie@thenewmotion.com>
2015-06-18 16:19:24 +0200

    Initial base functionality complete

--- fabdocker/docker.py
+++ fabdocker/docker.py
@@ -17,17 +17,14 @@ class Docker(object):
     def __call__(self, cmd, local=None):
         docker_cmd = "docker {}".format(cmd)
         if _check_local(local, env):
-            output = hide("stderr") if env.get("docker_verbose", False) else hide("stdout")
-            with output:
-                return run_local(docker_cmd, capture=True)
+            return run_local(docker_cmd, capture=True)
         else:
-            output = show("stdout", "stderr", "running") if env.get("docker_verbose", False) else hide("stdout", "stderr")
-            with output:
-                return run(docker_cmd)
+            return run(docker_cmd)
 
     def ps(self, all=False, local=None):
-        cmd = "ps --no-trunc"
-        cmd += " -a" if all else ""
+        cmd = "ps --no-trunc {all}".format(
+            all="-a" if all else ""
+        )
         result = self(cmd, local).replace("\r", "").split("\n")
         header = result[0]
         body = result[1:]
@@ -60,30 +57,108 @@ class Docker(object):
     def create(self, image, name=None, volumes=None, local=None):
         if not volumes:
             volumes = []
-        cmd = "create"
-        cmd += " --name {}".format(name) if name else ""
-        cmd += "".join([" -v {}".format(v) for v in volumes])
-        cmd += " {}".format(image)
+        cmd = "create {name} {volumes} {image}".format(
+            name="--name {}".format(name) if name else "",
+            volumes=" ".join(["-v {}".format(v) for v in volumes]),
+            image=image
+        )
         return self(cmd, local)
 
     def rm(self, container_id, force=False, local=None):
-        cmd = "rm"
-        cmd += "-f" if force else ""
-        cmd += " {}".format(container_id)
+        cmd = "rm {force} {container}".format(
+            force="-f" if force else "",
+            container=container_id
+        )
         return self(cmd, local)
 
     def inspect(self, container_id, fmt=None, local=None):
-        cmd = "inspect"
-        cmd += " --format='{}'".format(fmt) if fmt else ""
-        cmd += " {}".format(container_id)
+        cmd = "inspect {format} {container}".format(
+            format="--format='{}'".format(fmt) if fmt else "",
+            container=container_id
+        )
         result = self(cmd, local)
         return json.loads(result)
 
-    def run(self):
-        pass
+    def tag(self, source_image, target_image, source_tag=None, target_tag=None, force=None, local=None):
+        cmd = "tag {force} {source_image}{source_tag} {target_image}{target_tag}".format(
+            force="-f" if force else "",
+            source_image=source_image,
+            source_tag=":{}".format(source_tag) if source_tag else "",
+            target_image=target_image,
+            target_tag=":{}".format(target_tag) if target_tag else ""
+        )
+        return self(cmd, local)
 
-    def stop(self):
-        pass
+    def build(self, image, directory, tag=None, local=None):
+        cmd = "build -t {image}{tag} {directory}".format(
+            image=image,
+            tag=":{}".format(tag) if tag else "",
+            directory=directory
+        )
+        return self(cmd, local)
 
-    def replace(self):
-        pass
+    def pull(self, image, tag=None, local=None):
+        cmd = "pull {image}{tag}".format(
+            image=image,
+            tag=":{}".format(tag) if tag else ""
+        )
+        return self(cmd, local)
+
+    def login(self, url, username, password, email, local=None):
+        cmd = "login -u {user} -p {password} -e {email} {url}".format(
+            user=username,
+            password=password,
+            email=email,
+            url=url
+        )
+        return self(cmd, local)
+
+    def push(self, image, tag=None, local=None):
+        cmd = "push {image}{tag}".format(
+            image=image,
+            tag=":{}".format(tag) if tag else ""
+        )
+        return self(cmd, local)
+
+    def run(self, image, tag=None, name=None, ports=None, volumes=None, volumes_from=None, env_vars=None, daemon=True, local=None):
+        if not volumes:
+            volumes = {}
+        if not volumes_from:
+            volumes_from = []
+        if not env_vars:
+            env_vars = {}
+        if not ports:
+            ports = {}
+        cmd = "run {name} {daemon} {ports} {volumes} {volumes_from} {environment} {image}{version}".format(
+            name="--name {}".format(name) if name else "",
+            daemon="-d" if daemon else "",
+            ports=" ".join(["-p {}{}".format("{}:".format(v) if v else "", k) for k, v in ports.iteritems()]),
+            volumes=" ".join(["-v {}{}".format("{}:".format(v) if v else "", k) for k, v in volumes.iteritems()]),
+            volumes_from=" ".join(["--volumes-from {}".format(v) for v in volumes_from]),
+            environment=" ".join(["-e {}={}".format(k, v) for k, v in env_vars.iteritems()]),
+            image=image,
+            version=":{}".format(tag) if tag else ""
+        )
+        return self(cmd, local)
+
+    def stop(self, container_id, wait_time=None, local=None):
+        cmd = "stop {wait_time} {container}".format(
+            wait_time="-t {}".format(wait_time) if wait_time else "",
+            container=container_id
+        )
+        return self(cmd, local)
+
+    def replace(self, name, tag='latest', new_image=None, force=False, ports=None, volumes=None, volumes_from=None, env_vars=None, daemon=True, local=None):
+        containers = _filter_containers(self.ps(all=False, local=local), name=name)
+        if len(containers) > 0:
+            container_id = containers[0]['container_id']
+            image_name = new_image or containers[0]['image'].keys()[0]
+            if force:
+                self.rm(container_id, force=True, local=local)
+            else:
+                if self.inspect(container_id, fmt="{{ .State.Running }}", local=local):
+                    self.stop(container_id, local=local)
+                self.rm(container_id, local=local)
+        else:
+            image_name = new_image
+        return self.run(image_name, tag=tag, name=name, ports=ports, volumes=volumes, volumes_from=volumes_from, env_vars=env_vars, daemon=daemon, local=local)
